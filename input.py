37fd1d370a431dd735006fad3f6d8661e24a7c0c
Mihir Godbole <Mihir.chandrashekhar.godbole@one.verizon.com>
2019-07-22 14:18:28 -0400

    added input command exercise file

---
+++ input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+user_input = input("enter your IP4")
+print(f'You entered {user_input} as IP4 address')
+
+vendor_name = input("enter your vendor name")
+print("You entered {0} as vendor name".format(vendor_name))
+
+
---
+++ shebang.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#print(x="abc", end="!!!!")
+
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that's close enough.")
+print("No, I said ello, but that\'s close enough.")
