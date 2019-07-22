2863345a730b8b10a8ca10c67297b9b4d031fbe4
Mihir Godbole <Mihir.chandrashekhar.godbole@one.verizon.com>
2019-07-22 13:14:02 -0400

    added python print command exercise file

---
+++ print.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+x = "v1"
+y = "v2"
+z = "v3"
+a = "v4"
+
+print(x, y, z, a, sep="*", end="\n", file=sys.stdout, flush=False)
+
+print("192.168.30.5", "A1:02:CC:04", "255.255.255.0")
+
+a = 3.14
+print("a = ", a)
+
+print("a = \n", a)
+
+print ("a", "b")
+
+print("a", "b", sep="")
+
+print("a", "b", sep=".")
+
+print(192,168,178,42,sep=":)")
+
+print("I am thinking",end="... ... ... \n")
+
+print("I am thinking",end="!!!!")
+
+
